8f62d10d737877a60e7ce8d5d51d3e6bb19a975f
Gabriel <bielmarques922@gmail.com>
2021-09-27 09:43:14 -0300

    Variáveis

---
+++ 01-variaveis/script.js
@@ -0,0 +1,43 @@
+var nome = "Gabriel";
+var idade = 20;
+var faculdade = true;
+console.log(nome, idade, faculdade);
+
+var preco = 20;
+var totalComprado = 5;
+var totalPreco = totalComprado * preco;
+console.log(totalPreco);
+
+var sobreNome = "Marques",
+    trabalho = "Programador";
+console.log(sobreNome, trabalho);
+
+var semDefinir;
+console.log(semDefinir);
+
+// console.log(teste);
+
+
+
+/*  Exercicio */
+
+// Declarar uma variável com o seu nome
+var nome = "Gabriel";
+
+// Declarar uma variável com a sua idade
+var idade = 20;
+
+// Declarar uma variável com a sua comida favorita e não atribuir valor
+var comidaFavorita;
+
+// Atribuir valor a sua comida favorita
+comidaFavorita = "Lasanha";
+
+// Declarar 5 variáveis diferentes sem valores
+var hobbies = "Jogar",
+       trabalho,
+       pais,
+       rua,
+       animalFavorito;
+
+console.log(nome,idade, comidaFavorita);
