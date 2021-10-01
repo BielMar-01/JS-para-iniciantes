0ba5a57f37004951c274b4f7473e076032f3724a
Gabriel <bielmarques922@gmail.com>
2021-10-01 09:33:20 -0300

    Números e Operadores - Parte 1

---
+++ 03-numeros-e-operadores/script.js
@@ -0,0 +1,31 @@
+// NÚMEROS
+var idade = 28;
+var gols = 1000;
+var pi = 3.14; // ponto para decimal
+var exp = 2e10; // 20000000000
+// Precisão para até 15 digitos
+
+// OPERADORES ARITMÉTICOS
+var soma = 100 + 50; // 150
+var subtracao = 100 - 50; // 50
+var multiplicacao = 100 * 2; // 200
+var divisao = 100 / 2; // 50
+var expoente = 2 ** 4; // 16
+var modulo = 14 % 5; // 4
+// Lembrando que soma + em Strings serve para concatenar
+
+
+// OPERADORES ARITMÉTICOS (STRINGS)
+var soma = '100' + 50; // 10050
+var subtracao = '100' - 50; // 50
+var multiplicacao = '100' * '2'; // 200
+var divisao = 'Comprei 10' / 2; // NaN (Not a Number)
+// É possível verificar se uma variável é NaN ou não com a função isNaN()
+
+
+// NAN = NOT A NUMBER
+var numero = 80;
+var unidade = 'kg';
+var peso = numero + unidade; // '80kg'
+var pesoPorDois = peso / 2 // NaN (Not a Number)
+
