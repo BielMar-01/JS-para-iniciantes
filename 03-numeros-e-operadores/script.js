63cfdb30ba50d53f4ee2300b7d62f67402bb28b3
Gabriel <bielmarques922@gmail.com>
2021-10-04 09:06:43 -0300

    Números e Operadores - Parte 2

--- 03-numeros-e-operadores/script.js
+++ 03-numeros-e-operadores/script.js
@@ -29,3 +29,71 @@ var unidade = 'kg';
 var peso = numero + unidade; // '80kg'
 var pesoPorDois = peso / 2 // NaN (Not a Number)
 
+
+// A ORDEM IMPORTA
+// Começa por multiplicação e divisão, depois por soma e subtração.
+var total1 = 20 + 5 * 2; // 30
+var total2 = (20 + 5) * 2; // 50
+var total3 = 20 / 2 * 5; // 50
+var total4 = 10 + 10 * 2 + 20 / 2; // 40
+// Parênteses para priorizar uma expressão
+
+
+// OPERADORES ARITMÉTICOS UNÁRIOS
+var incremento = 5;
+console.log(incremento++); // 5
+console.log(incremento); // 6
+
+var incremento2 = 5;
+console.log(++incremento2); // 6
+console.log(incremento2); // 6
+// Mesma coisa para o decremento --x
+
+
+// OPERADORES ARITMÉTICOS UNÁRIOS
+// O + e - tenta transformar o valor seguinte em número
+var frase = 'Isso é um teste';
++frase; // NaN
+-frase; // NaN
+
+var idade = '28';
++idade; // 28 (número)
+-idade; // -28 (número)
+console.log(+idade + 5); // 33 
+
+var possuiFaculdade = true;
+console.log(+possuiFaculdade); // 1
+// O - antes de um número torna ele negativo
+
+
+// EXERCÍCIO
+// Qual o resultado da seguinte expressão?
+var total = 10 + 5 * 2 / 2 + 20;
+console.log(total);
+
+// Crie duas expressões que retornem NaN
+var expressao1 = 'Teste' / 2;
+var expressao2 = 'Teste' - 2;
+console.log(expressao1, expressao2);
+
+// Somar a string '200' com o número 50 e retornar 250
+var soma = '200' + 50;
+console.log(soma);
+var soma1 = +'200' + 50;
+console.log(soma1)
+
+// Incremente o número 5 e retorne o seu valor incrementado
+var x = 5;
+console.log(x);
+x++;
+console.log(x)
+
+// Como dividir o peso por 2?
+var numero = '80';
+var unidade = 'kg';
+var peso = numero + unidade; // '80kg'
+var pesoPorDois = peso / 2; // NaN (Not a Number)
+console.log(pesoPorDois);
+
+numero = +'80' / 2;
+console.log(numero);
