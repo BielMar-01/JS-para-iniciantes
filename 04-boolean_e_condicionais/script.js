533b1269c01a9d81a220f090f3313cff39f70149
Gabriel <bielmarques922@gmail.com>
2021-10-06 09:52:29 -0300

    Boolean e Condicionais - Parte 2

--- 04-boolean_e_condicionais/script.js
+++ 04-boolean_e_condicionais/script.js
@@ -58,25 +58,6 @@ if({})
 */
 
 
-// SWITCH
-// Com o switch você pode verificar se uma variável é igual à diferentes valores utilizando o case. Caso ela seja igual, você pode fazer alguma coisa e utilizar a palavra chave break; para cancelar a continuação. O valor de default ocorrerá caso nenhuma das anteriores seja verdadeira.
-var corFavorita = 'Azul';
-
-switch (corFavorita) {
-  case 'Azul':
-    console.log('Olhe para o céu.');
-    break;
-  case 'Vermelho':
-    console.log('Olhe para rosas.');
-    break;
-  case 'Amarelo':
-    console.log('Olhe para o sol.');
-    break;
-  default:
-    console.log('Feche os olhos');
-}
-
-
 // OPERADOR LÓGICO DE NEGAÇÃO !
 // O operador !, nega uma operação booleana. Ou seja, !true é igual a false
 // Truthy
@@ -113,3 +94,110 @@ if(!!'') // false
 10 != '10' // false
 10 !== '10' // true
 */
+
+
+// OPERADORES LÓGICOS &&
+// && Compara se uma expressão e a outra é verdadeira
+/* 
+true && true; // true
+true && false; // false
+false && true; // false
+'Gato' && 'Cão'; // 'Cão'
+(5 - 5) && (5 + 5); // 0
+'Gato' && false; // false
+(5 >= 5) && (3 < 6); // true
+*/
+// Se ambos os valores forem true ele irá retornar o último valor verificado Se algum valor for false ele irá retornar o mesmo e não irá continuar a verificar os próximos
+
+
+// OPERADORES LÓGICOS ||
+// || Compara se uma expressão ou outra é verdadeira
+/*
+true || true; // true
+true || false; // true
+false || true; // true
+'Gato' || 'Cão'; // 'Gato'
+(5 - 5) || (5 + 5); // 10
+'Gato' || false; // Gato
+(5 >= 5) || (3 < 6); // true
+*/
+// Retorna o primeiro valor true que encontrar
+
+
+// SWITCH
+// Com o switch você pode verificar se uma variável é igual à diferentes valores utilizando o case. Caso ela seja igual, você pode fazer alguma coisa e utilizar a palavra chave break; para cancelar a continuação. O valor de default ocorrerá caso nenhuma das anteriores seja verdadeira.
+var corFavorita = 'Azul';
+
+switch (corFavorita) {
+  case 'Azul':
+    console.log('Olhe para o céu.');
+    break;
+  case 'Vermelho':
+    console.log('Olhe para rosas.');
+    break;
+  case 'Amarelo':
+    console.log('Olhe para o sol.');
+    break;
+  default:
+    console.log('Feche os olhos');
+}
+
+
+// EXERCÍCIO
+// Verifique se a sua idade é maior do que a de algum parente
+// Dependendo do resultado coloque no console 'É maior', 'É igual' ou 'É menor'
+console.log('=====Exercício 01======');
+var minhaIdade = 20;
+var idadeParente = 20;
+
+if(minhaIdade === idadeParente) {
+  console.log("É igual");
+} else if(minhaIdade > idadeParente){
+  console.log("É maior");
+} else {
+  console.log("É menor");
+}
+
+// Qual valor é retornado na seguinte expressão?
+console.log('=====Exercício 02======');
+var expressao = (5 - 2) && (5 - ' ') && (5 - 2);
+console.log(expressao);
+
+// Verifique se as seguintes variáveis são Truthy ou Falsy
+console.log('=====Exercício 03======');
+var nome = 'Andre';
+var idade = 28;
+var possuiDoutorado = false;
+var empregoFuturo;
+var dinheiroNaConta = 0;
+
+console.log(!!nome, !!idade, !!possuiDoutorado, !!empregoFuturo, !!dinheiroNaConta);
+
+// Compare o total de habitantes do Brasil com China (valor em milhões)
+console.log('=====Exercício 04======');
+var brasil = 207;
+var china = 1340;
+
+if (brasil > china) {
+  console.log('Brasil tem mais habitantes');
+} else {
+  console.log('Brasil tem menos habitantes');
+}
+
+// O que irá aparecer no console?
+console.log('=====Exercício 05======');
+if(('Gato' === 'gato') && (5 > 2)) {
+  console.log('Verdadeiro');
+} else {
+  console.log('Falso');
+}
+// Falso
+
+// O que irá aparecer no console?
+console.log('=====Exercício 06======');
+if(('Gato' === 'gato') || (5 > 2)) {
+  console.log('Gato' && 'Cão');
+} else {
+  console.log('Falso');
+}
+// Cão
