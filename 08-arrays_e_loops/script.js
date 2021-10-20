e15f8a8822128416a6be0ea4758f371dcbce78c6
Gabriel <bielmarques922@gmail.com>
2021-10-20 09:58:33 -0300

    Arrays e Loops - Parte 2

--- 08-arrays_e_loops/script.js
+++ 08-arrays_e_loops/script.js
@@ -33,3 +33,64 @@ while (i < 10) {
 // Retorna de 0 a 9 no console
 // O for loop é o mais comum
 
+
+// ARRAYS E LOOPS
+var videoGames = ['Switch', 'PS4', 'XBox', '3DS'];
+for (var i = 0; i < videoGames.length; i++) {
+  console.log(videoGames[i]);
+}
+// O for loop é o mais comum
+
+
+// BREAK
+// O loop irá parar caso encontro e palavra break
+var videoGames = ['Switch', 'PS4', 'XBox', '3DS'];
+for (var i = 0; i < videoGames.length; i++) {
+  console.log(videoGames[i]);
+  if(videoGames[i] === 'PS4') {
+    break;
+  }
+}
+
+
+// FOREACH
+// forEach é um método que executa uma função para cada item da Array. É uma forma mais simples de utilizarmos um loop com arrays (ou array-like)
+var videoGames = ['Switch', 'PS4', 'XBox', '3DS'];
+videoGames.forEach(function(item) {
+  console.log(item);
+});
+// O argumento item será atribuído dinamicamente
+// Podemos passar os seguintes parâmetros item, index e array
+
+
+// NÃO SE CONFUNDA COM A SINTAXE
+var numero = 0;
+var maximo = 50;
+for(;numero < maximo;) {
+  console.log(numero);
+  numero++;
+}
+// Não aconselho escrever da forma acima, mas funciona normalmente.
+
+
+// EXERCÍCIO
+// Crie uma array com os anos que o Brasil ganhou a copa 1959, 1962, 1970, 1994, 2002
+var ano = [1959, 1962, 1970, 1994, 2002];
+
+// Interaja com a array utilizando um loop, para mostrar no console a seguinte mensagem, `O brasil ganhou a copa de ${ano}`
+for(var i = 0; i < ano.length;  i++){
+  console.log(`O brasil ganhou a copa de ${ano[i]}`);
+}
+
+// Interaja com um loop nas frutas abaixo e pare ao chegar em Pera
+var frutas = ['Banana', 'Maçã', 'Pera', 'Uva', 'Melância'];
+for (var i = 0; i < frutas.length; i++) {
+  console.log(frutas[i]);
+  if(frutas[i] === 'Pera') {
+    break;
+  }
+}
+
+// Coloque a última fruta da array acima em uma variável, sem remover a mesma da array.
+var ultimaFruta = frutas[frutas.length - 1];
+console.log(ultimaFruta);
